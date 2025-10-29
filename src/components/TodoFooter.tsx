69530e9adb44a4238dc7fb9b2fe5c9db9501ef73
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-10-29 20:15:11 +0200

    TodoApp: loading todos

---
+++ src/components/TodoFooter.tsx
@@ -0,0 +1,33 @@
+import { TodoNavigatoin } from "./TodoNavigation";
+
+import { Status } from "../types/StatusEnum";
+import React from "react";
+import { Todo } from "../types/Todo";
+
+type Props = {
+  status: Status;
+  completedTodos: Todo[],
+  onSwitch: (status: Status) => void;
+};
+
+export const TodoFooter: React.FC<Props> = ({ status, completedTodos, onSwitch }) => {
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {completedTodos.length} items left
+      </span>
+
+      {/* Active link should have the 'selected' class */}
+      <TodoNavigatoin status={status} onSwitch={onSwitch} />
+
+      {/* this button should be disabled if there are no completed todos */}
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
